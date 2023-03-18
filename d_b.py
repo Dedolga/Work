56f6df22319edf4f01f0adeeb39e1aae77dae3a8
Dedolga <120402084+Dedolga@users.noreply.github.com>
2023-03-18 21:11:38 +0300

    Add files via upload

--- d_b.py
+++ d_b.py
@@ -1,7 +1,8 @@
 import psycopg2
+from mains import *
 
-conn = psycopg2.connect(database='', user='postgres', password='')
-
+conn = psycopg2.connect(database='data_bd', user='postgres', password='90Olgabase23')
+conn.autocommit = True
 
 def create_table_userdata():
     with conn.cursor() as cur:
@@ -12,66 +13,41 @@ def create_table_userdata():
             vk_id varchar(30) not null primary key, 
             vk_link varchar(60));"""
         )
-        conn.commit()
-    print('Таблица с пользователями создана')
-
 
 def create_table_processed_users():
     with conn.cursor() as cur:
         cur.execute(
             """CREATE TABLE IF NOT EXISTS processed_users(
             id serial,
-            vk_id varchar(60) primary key);"""
+            puvk_id varchar(60) primary key);"""
         )
-        conn.commit()
-    print('Таблица с обработанными пользователями создана')
-
 
 def insert_userdata(first_name, last_name, vk_id, vk_link):
     with conn.cursor() as cur:
         cur.execute(
                 f"""INSERT INTO userdata (first_name, last_name, vk_id, vk_link) 
-            VALUES ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}');"""
+            VALUES ('{first_name}', '{last_name}', '{vk_id}', '{vk_link}') ON CONFLICT(vk_id) DO UPDATE SET
+            first_name = '{first_name}', last_name = '{last_name}', vk_link = '{vk_link}';"""
             )
-        conn.commit()
     print('Данные в таблицу пользователей успешно внесены')
 
-def insert_processed_users(vk_id, offset):
+def insert_processed_users(puvk_id, offset):
     with conn.cursor() as cur:
-        cur.execute(f"""INSERT INTO processed_users (vk_id) 
-            VALUES ('{vk_id}')
+        cur.execute(
+            f"""INSERT INTO processed_users (puvk_id) 
+            VALUES ('{puvk_id}')
             OFFSET '{offset}';"""
         )
-        conn.commit()
     print('Данные в таблицу обработанных пользователей успешно внесены')
 
 def select(offset):
     with conn.cursor() as cur:
-        cur.execute(f"""SELECT u.first_name, u.last_name, u.vk_id, u.vk_link, pu.vk_id FROM userdata AS u
-                        LEFT JOIN processed_users AS pu ON u.vk_id = pu.vk_id WHERE pu.vk_id IS NULL OFFSET '{offset}';""")
+        cur.execute(f"""SELECT u.first_name, u.last_name, u.vk_id, u.vk_link FROM userdata AS u
+                        LEFT JOIN processed_users AS pu ON u.vk_id = pu.puvk_id WHERE pu.puvk_id IS NULL OFFSET '{offset}';""")
         return cur.fetchone()
 
 
-def drop_table_userdata():
-    with conn.cursor() as cur:
-        cur.execute(
-            """DROP TABLE IF EXISTS userdata CASCADE;"""
-        )
-        conn.commit()
-        print('Таблица с пользователями удалена')
-
-
-def drop_table_processed_users():
-    with conn.cursor() as cur:
-        cur.execute(
-            """DROP TABLE  IF EXISTS processed_users CASCADE;"""
-        )
-        conn.commit()
-        print('Таблица с обработанными пользователями удалена')
-
-
 def creating_database():
-    drop_table_userdata()
-    drop_table_processed_users()
     create_table_userdata()
     create_table_processed_users()
+
